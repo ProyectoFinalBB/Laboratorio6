79b5d80f706663e3e2b5e82eb8040df4583d1f4c
NexPro <proyectofinalbb@gmail.com>
2024-06-08 19:14:29 -0300

    Cambios en el Lobby

--- lobby/lobby.php
+++ lobby/lobby.php
@@ -1,9 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorios</title>
+    <link rel="stylesheet" href="../laboratorio/styles.css"> <!-- Ruta correcta al archivo CSS -->
+</head>
 <body>
-
-<a href="">Laboratorio 1</a>
-<a href="">Laboratorio 2</a>
-<a href="">Laboratorio 3</a>
-<a href="">Laboratorio 4</a>
-<a href="">Laboratorio 5</a>
-
+    <div class="contenC">
+        <div class="container">
+            <a href="" class="button">Laboratorio 1</a>
+            <a href="" class="button">Laboratorio 2</a>
+            <a href="" class="button">Laboratorio 3</a>
+            <a href="" class="button">Laboratorio 4</a>
+            <a href="" class="button">Laboratorio 5</a>
+        </div>
+    </div>
 </body>
+</html>
