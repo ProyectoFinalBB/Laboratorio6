baed92a8c762a6f279694f800d044841947c5cfe
AlexCarnelli <agcarnelli2023@gmail.com>
2024-06-08 18:31:22 -0300

    lobby creado

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ session_start();
 if (isset($_SESSION['username'])) {
 
     include("header.php");
-    
+    include("lobby/lobby.php");
     include("footer.php");
 } else {
     include("login.php");
---
+++ lobby/lobby.php
@@ -0,0 +1,9 @@
+<body>
+
+<a href="">Laboratorio 1</a>
+<a href="">Laboratorio 2</a>
+<a href="">Laboratorio 3</a>
+<a href="">Laboratorio 4</a>
+<a href="">Laboratorio 5</a>
+
+</body>
